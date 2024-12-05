b56b559b6fd5d40da455800b60bfe60126567e6b
Hasan-TechNinja <hasantechninja@gmail.com>
2024-12-05 17:51:41 +0600

    upgrade the app name

--- NPR/wsgi.py
+++ NPR/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Review project.
+WSGI config for NPR project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Review.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NPR.settings')
 
 application = get_wsgi_application()
